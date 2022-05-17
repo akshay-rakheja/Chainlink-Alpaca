f779dec33e57d390de81c84586307f608279114d
Akshay Rakheja <a3rakhej@uwaterloo.ca>
2022-05-17 16:53:44 -0400

    Modified tradeEquities to take in a particular side of the market

--- app.js
+++ app.js
@@ -18,28 +18,8 @@ const port = process.env.EA_PORT || 8080;
 const host = process.env.EA_HOST || "172.17.0.1";
 
 app.use(bodyParser.json());
-
-app.post("/cryptoaskingsize", async (req, res) => {
-  const { status, result } = await getCryptoAskingSize(req.body);
-  res.status(status).json(result);
-});
-app.post("/cryptoprice", async (req, res) => {
-  const { status, result } = await getCryptoPrice(req.body);
-  res.status(status).json(result);
-});
-app.post("/cryptotrade", async (req, res) => {
-  const { status, result } = await tradeCrypto(req.body);
-  res.status(status).json(result);
-});
-app.post("/equitiestrade", async (req, res) => {
-  const { status, result } = await tradeEquities(req.body);
-  res.status(status).json(result);
-});
-app.post("/equitiesprice", async (req, res) => {
-  const { status, result } = await getEquitiesPrice(req.body);
-  res.status(status).json(result);
-});
 app.listen(port, host, () => console.log(`Listening on port ${port}!`));
+
 //#endregion
 
 //#region requests
@@ -120,15 +100,15 @@ const tradeCrypto = async (input) => {
 //#endregion
 
 
-// @TODO: Trade Equities on Alpaca
 // Trade equities on Alpaca for the given symbol
 const tradeEquities = async (input) => {
   const jobRunId = typeof input.id === "undefined" ? 1 : input.id;
   try {
-    const { symbol, qty } = input.data;
+    const { symbol, qty , side} = input.data;
     if (!symbol) throw new Error("Symbol is required");
     if (!qty) throw new Error("Quantity is required");
-    const body = {symbol, qty, side:'buy', type:"market", time_in_force:"day"};
+    if (!side) throw new Error("Buy/Sell Side is required");
+    const body = {symbol, qty, side, type:"market", time_in_force:"day"};
     const url = `https://paper-api.alpaca.markets/v2/orders`;
     const response = await fetch(url, { headers, body: JSON.stringify(body), method: 'POST' });
     const data = await response.json();
@@ -158,7 +138,7 @@ const getEquitiesPrice = async (input) => {
     const url = `https://data.alpaca.markets/v2/stocks/${symbol}/quotes/latest`;
     const response = await fetch(url, { headers });
     const data = await response.json();
-    const price = Math.floor(data.quote.ap * 100);
+    const price =data.quote.ap;
     return {
       status: response.status,
       result: { jobRunId, price },
@@ -173,3 +153,24 @@ const getEquitiesPrice = async (input) => {
   }
 };
 //#endregion
+
+app.post("/cryptoaskingsize", async (req, res) => {
+  const { status, result } = await getCryptoAskingSize(req.body);
+  res.status(status).json(result);
+});
+app.post("/cryptoprice", async (req, res) => {
+  const { status, result } = await getCryptoPrice(req.body);
+  res.status(status).json(result);
+});
+app.post("/cryptotrade", async (req, res) => {
+  const { status, result } = await tradeCrypto(req.body);
+  res.status(status).json(result);
+});
+app.post("/equitiestrade", async (req, res) => {
+  const { status, result } = await tradeEquities(req.body);
+  res.status(status).json(result);
+});
+app.post("/equitiesprice", async (req, res) => {
+  const { status, result } = await getEquitiesPrice(req.body);
+  res.status(status).json(result);
+});
